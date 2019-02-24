e831c0f69df31f17a9b37df189638d7efbc4b626
Oleh <oifilonchuk@gmail.com>
2019-02-24 22:49:11 +0200

    add header

--- src/components/Main-banner/index.js
+++ src/components/Main-banner/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import './Main-banner.css'
 import star1 from './img/star1.svg'
 import star2 from './img/star2.svg'
+import logo from './img/owl-logo.PNG'
+import ring from './img/big-circle.svg'
 
 export default class SectionOne extends Component {
   render() {
@@ -15,6 +17,18 @@ export default class SectionOne extends Component {
         <img src={star2} alt="" className="star star6 star-secondary"/>
         <img src={star2} alt="" className="star star7 star-secondary"/>
         <img src={star2} alt="" className="star star8 star-secondary"/>
+        
+        <header>
+          <h1>FPR?</h1>
+          <div className="logo-container">
+            <img src={logo} alt="" className="logo"/>
+          </div>
+          <a href="#">Contact</a>
+        </header>
+
+        <div className="ring">
+          <img src={ring} alt=""/>
+        </div>
       </div>
     )
   }
