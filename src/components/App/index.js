9919c344426ce43cdeb20f14befea4bb7d5d8896
Oleh <oifilonchuk@gmail.com>
2019-02-24 20:37:44 +0200

    started to create own site

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import SectionOne from '../Main-banner'
 import './App.css'
 
 export default class App extends Component {
   render() {
     return (
       <div className="container">
-        App
+        <SectionOne />
       </div>
     )
   }
---
+++ src/components/Main-banner/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './Main-banner.css'
+import star1 from './img/star1.svg'
+
+export default class SectionOne extends Component {
+  render() {
+    return (
+      <div className="main-banner">
+        <img src={star1} alt="star"/>
+        <img src="" alt=""/>
+        <img src="" alt=""/>
+        <img src="" alt=""/>
+        <img src="" alt=""/>
+        <img src="" alt=""/>
+      </div>
+    )
+  }
+}
