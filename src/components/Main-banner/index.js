0fb809d34162f69564740f6de9c6b7d891314206
Oleg Filonchuk <oifilonchuk@gmail.com>
2019-02-26 12:47:42 +0200

    first section almost finished

--- src/components/Main-banner/index.js
+++ src/components/Main-banner/index.js
@@ -9,22 +9,31 @@ export default class SectionOne extends Component {
   render() {
     return (
       <div className="main-banner">
-      <img src={star1} alt="" className="star star1"/>
-      <img src={star1} alt="" className="star star2"/>
-      <img src={star1} alt="" className="star star3"/>
-      <img src={star1} alt="" className="star star4"/>
+        <img src={star1} alt="" className="star star1"/>
+        <img src={star1} alt="" className="star star2"/>
+        <img src={star1} alt="" className="star star3"/>
+        <img src={star1} alt="" className="star star4"/>
         <img src={star2} alt="" className="star star5 star-secondary"/>
         <img src={star2} alt="" className="star star6 star-secondary"/>
         <img src={star2} alt="" className="star star7 star-secondary"/>
         <img src={star2} alt="" className="star star8 star-secondary"/>
         
         <header>
-        <h1>FPR?</h1>
-          <div className="logo-container">
-          <img src={logo} alt="" className="logo"/>
+          <h1>FPR?</h1>
+          <div className="main-banner__logo-container">
+            <img src={logo} alt="" className="logo"/>
           </div>
-          <a href="#">Contact</a>
+          <span className="contact-link">Contact</span>
         </header>
+
+        <div className="main-banner__content">
+          <h1>Landing Pages for free mafaka</h1>
+          <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente totam expedita quaerat, dolor pariatur ducimus voluptas sit est ad veniam magni adipisci aperiam.</h2>
+        </div>
+
+        <a href="/" className="main-banner__button">
+          Sounds good? Let's chat
+        </a>
         
         <div className="ring-container">
           <div className="ring">
