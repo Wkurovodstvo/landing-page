f5e6c7e4e85e85762205e5e928f850f5f1bec770
Oleg Filonchuk <oifilonchuk@gmail.com>
2019-02-27 13:52:36 +0200

    fifth screen in process

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import './Social.css'
 
 export default class Social extends Component {
   render() {
     return (
       <div className="social">
         <div className="social__follow">
-          
+          <h4>Follow me</h4>
+          <ul>
+            <a href="/" className="social__link dribble"><li>dribbble</li></a>
+            <a href="/" className="social__link behance"><li>behance</li></a>
+            <a href="/" className="social__link facebook"><li>facebook</li></a>
+            <a href="/" className="social__link twitter"><li>twitter</li></a>
+            <a href="/" className="social__link instagram"><li>instagram</li></a>
+            <a href="/" className="social__link medium"><li>medium</li></a>
+          </ul>
         </div>
 
         <div className="social__describe">
