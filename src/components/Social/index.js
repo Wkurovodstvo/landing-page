020b352d45896078b89b14392950fc55bec8d244
Oleg Filonchuk <oifilonchuk@gmail.com>
2019-02-28 13:07:22 +0200

    add social

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -18,7 +18,7 @@ export default class Social extends Component {
         </div>
 
         <div className="social__describe">
-        
+          Great design isn’t just my job, it’s also my passion. I married <a href="/" className="social__describe_link">another designer</a>, for pete’s sake! I write about design on <a href="/" className="social__describe_link">Medium</a>, and share my work on my <a href="/" className="social__describe_link">Instagram</a>, <a href="/" className="social__describe_link">Dribble</a>, and <a href="/" className="social__describe_link">Behance</a> accounts. I also share work and articles that inspire me on <a href="/" className="social__describe_link">Twitter</a> and <a href="/" className="social__describe_link">Facebook</a>. If you want to follow along or <a href="/" className="social__describe_link">say hi</a>, please do!
         </div>
       </div>
     )
