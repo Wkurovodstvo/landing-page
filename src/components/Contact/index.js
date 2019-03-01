4dd6d09d65f716c2c88f43bcf66582eb2da833a3
Oleg Filonchuk <oifilonchuk@gmail.com>
2019-03-01 09:29:53 +0200

    form processed

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -10,29 +10,29 @@ export default class Contact extends Component {
 
           <form action="" id="contact-form">
 
-            <div className="half">
+            <div className="contact-div half">
               <label htmlFor="name">Name *</label>
-              <input type="text" id="name" placeholder="Your name"/>
+              <input type="text" id="name" placeholder="Your name" className="contact-input"/>
             </div>
 
-            <div className="half">
+            <div className="contact-div half">
               <label htmlFor="organization">Organization</label>
-              <input type="text" id="organization" placeholder="Your organization"/>
+              <input type="text" id="organization" placeholder="Your organization" className="contact-input"/>
             </div>
 
-            <div className="half">
+            <div className="contact-div full">
               <label htmlFor="email">Email address *</label>
-              <input type="text" id="email" placeholder="Your email"/>
+              <input type="text" id="email" placeholder="Your email" className="contact-input"/>
             </div>
 
-            <div className="half">
+            <div className="contact-div full">
               <label htmlFor="desription">Project Description *</label>
-              <textarea name="description" id="description"></textarea>
+              <textarea name="description" id="description" className="contact-input"></textarea>
             </div>
 
-            <div className="half">
+            <div className="contact-div full">
               <label htmlFor="budget">Budget</label>
-              <select name="budget" id="budget">
+              <select name="budget" id="budget" className="contact-input">
                 <option value="0">За спасибо</option>
                 <option value="4">4 Шаурмы</option>
                 <option value="8">8 Шаурмы</option>
@@ -40,23 +40,23 @@ export default class Contact extends Component {
               </select>
             </div>
             
-            <div className="half">
+            <div className="contact-div full">
              <label htmlFor="find">How did you find me?</label>
-              <input type="text" id="find" placeholder="Ex. Dribble"/>
+              <input type="text" id="find" placeholder="Ex. Dribble" className="contact-input"/>
             </div>
 
-            <div className="half">
+            <div className="contact-div half">
               <label htmlFor="kickoff">Ideal kickoff date</label>
-              <input type="text" id="kickoff" placeholder="Ex. 2 weeks"/>
+              <input type="text" id="kickoff" placeholder="Ex. 2 weeks" className="contact-input"/>
             </div>
 
-            <div className="half">
+            <div className="contact-div half">
               <label htmlFor="launch">Ideal launch date</label>
-              <input type="text" id="launch" placeholder="Ex. January 15th"/>
+              <input type="text" id="launch" placeholder="Ex. January 15th" className="contact-input"/>
             </div>
 
-            <div className="half">
-              <input type="submit" value="Submit"/>
+            <div className="contact-div full">
+              <input type="submit" value="Submit" className="contact-input"/>
             </div>
           </form>
         </div>
